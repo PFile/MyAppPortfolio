e0dda27172d951b8439996f26ebba545696f78b4
phantasy <aoanzz@outlook.com>
2016-06-01 19:19:37 +0800

    wrote a app

---
+++ app/src/main/java/com/example/administrator/myappportfolio/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void show(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),"Popular Movie Message",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+}
